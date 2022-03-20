c56fd6e8eb12bee011924496c8ce76df4d19c8ff
Roman Erdt <roman.erdt@gmail.com>
2022-03-20 18:21:48 +0200

    introducing hooksx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import ReactDOM from 'react-dom';
-import Pet from './Pet';
+import ReactDOM from "react-dom";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cocktail" />
-      <Pet name="Sudo" animal="Dog" breed="Terrier" />
+      <SearchParams />
     </div>
-  )
-}
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,12 +1,4 @@
-import React from 'react';
-
-// const Pet = (props) => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h2", {}, props.name),
-//     React.createElement("h3", {}, props.animal),
-//     React.createElement("h3", {}, props.breed),
-//   ]);
-// };
+import React from "react";
 
 const Pet = (props) => {
   return (
@@ -15,7 +7,7 @@ const Pet = (props) => {
       <h3>{props.animal}</h3>
       <h3>{props.breed}</h3>
     </div>
-  )
-}
+  );
+};
 
 export default Pet;
